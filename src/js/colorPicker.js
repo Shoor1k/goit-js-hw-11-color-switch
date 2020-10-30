98fa20656eb7bf7a1bcd35aa1f60cb4bf3cbb95f
Shoor1k <qipshoor1k@gmail.com>
2020-10-30 15:19:13 +0200

    done

---
+++ src/js/colorPicker.js
@@ -0,0 +1,44 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let intervalChangeColor = null;
+
+const refs = {
+start : document.querySelector(`button[data-action="start"]`),
+stop: document.querySelector(`button[data-action="stop"]`),
+section: document.querySelector(`body`)
+}
+
+refs.start.addEventListener('click', onStartChangeColor)
+refs.stop.addEventListener('click', onStopChangeColor)
+
+
+function onStartChangeColor() {
+    const TIME_CHANGE_COLOR = 1000
+    
+    refs.start.setAttribute(`disabled`, "")
+    
+    intervalChangeColor = setInterval(() => {
+    refs.section.setAttribute(`bgcolor`, `${colors[randomIntegerFromInterval(1, 6)]}`)
+    
+}, TIME_CHANGE_COLOR);
+    
+}
+
+function onStopChangeColor() {
+    clearInterval(intervalChangeColor)
+    refs.start.removeAttribute(`disabled`)
+}
+
+
+
